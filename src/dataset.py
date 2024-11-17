9a635ee87b837c474c653867d532f4af3b964666
Aryan <82381989+akumm2k@users.noreply.github.com>
2024-11-17 19:55:26 +0100

    Add loading method for michals22 coffee dataset

--- src/dataset.py
+++ src/dataset.py
@@ -10,22 +10,22 @@ LOGGER = logging.getLogger(__name__)
 class BaseDataset:
     _KAGGLE_DATASET_PATH: str = None
     _FILE_YEAR_PATTERN = re.compile(r"WHR_(\d{4}).csv")
-    
+
     _IS_TIME_SERIES = False
-    
+
     def __init__(self, data: pd.DataFrame) -> None:
         self._data = data
-        
+
         if self._IS_TIME_SERIES:
             self._years = sorted(
                 list(data.keys())
             )
-    
+
     def get_years(self) -> List[str]:
         if not self._IS_TIME_SERIES:
             raise ValueError("This dataset is not a time series dataset.")
         return self._years
-    
+
     def __getitem__(self, year: int) -> pd.DataFrame:
         if not self._IS_TIME_SERIES:
             raise ValueError("This dataset is not a time series dataset.")
@@ -39,7 +39,7 @@ class BaseDataset:
             f" Got {year_str}."
         )
         return self._data[year_str]
-    
+
 class HappinessDataset(BaseDataset):
     _KAGGLE_DATASET_PATH: str = (
         "sazidthe1/global-happiness-scores-and-factors"
@@ -73,7 +73,7 @@ class HappinessDataset(BaseDataset):
         LOGGER.info(f"Loaded data for years: {data.get_years()}")
 
         return data
-    
+
 class AlcoholDataset(BaseDataset):
     _KAGGLE_DATASET_PATH: str = (
         "thedevastator/oecd-alcohol-consumption-per-capita/versions/2"
@@ -120,7 +120,7 @@ class MarriageDataset(BaseDataset):
     _KAGGLE_DATASET_PATH: str = (
         "johnny1994/divorce-rates-data-should-you-get-married"
     )
-        
+
     def __getitem__(self, key: str) -> pd.DataFrame:
         assert key in ['DATA1', 'DATA2'], (
             f"Key must be one of ['DATA1', 'DATA2']. Got {key}."
@@ -138,25 +138,25 @@ class MarriageDataset(BaseDataset):
 
         csv_names = os.listdir(path)
         LOGGER.debug(f"Found CSV files: {csv_names}")
-        
+
         # Read specific sheets
         sheet1 = pd.read_excel(f'{path}/{csv_names[0]}', sheet_name='Divorce statistics by country_r')
         sheet2 = pd.read_excel(f'{path}/{csv_names[0]}', sheet_name='Estimates of annual divorces by')
-        
+
         sheets = {
             'DATA1': sheet1,
             'DATA2': sheet2
         }
-        
+
         data = cls(sheets)
         return data
-    
+
 class CoffeeDataset(BaseDataset):
     _KAGGLE_DATASET_PATH: str = (
-        "waqi786/worldwide-coffee-habits-dataset"
+        "michals22/coffee-dataset"
     )
     _FILE_YEAR_PATTERN = re.compile(r"WHR_(\d{4}).csv")
-    _YEARS = ['2015', '2016', '2017', '2018', '2019', '2020', '2021', '2022', '2023']
+    _YEARS = ['2015', '2016', '2017', '2018', '2019']
     _IS_TIME_SERIES = True
 
     @classmethod
@@ -171,22 +171,29 @@ class CoffeeDataset(BaseDataset):
         csv_names = os.listdir(path)
         LOGGER.debug(f"Found CSV files: {csv_names}")
         year_to_df = {}
-        for csv_name in csv_names:
-            data = pd.read_csv(f'{path}/{csv_name}')
 
-            # Drop the 'INDEX' column if it exists
-            if 'index' in data.columns:
-                data = data.drop(columns=['index'])
+        csv_name = f"{path}/Coffee_domestic_consumption.csv"
+        LOGGER.debug(f"Reading CSV file: {csv_name}")
 
-            for year in cls._YEARS:
-                if int(year) in data["Year"].values:
-                    if year not in year_to_df:
-                        year_to_df[year] = data[data['Year'] == int(year)].reset_index(drop=True)
-                    else:
-                        year_to_df[year] = pd.concat(
-                            [year_to_df[year], data[data['Year'] == int(year)].reset_index(drop=True)],
-                            ignore_index=True
-                        )
+        data = pd.read_csv(csv_name)
+
+        # Drop the 'INDEX' column if it exists
+        if 'index' in data.columns:
+            data = data.drop(columns=['index'])
+
+        data.columns = [
+            re.sub(r"(\d{4})/\d{2}", r"\1", col) if re.match(r"(\d{4})/\d{2}", col) else col
+            for col in data.columns
+        ]
+
+        for year in cls._YEARS:
+            if year in data.columns:
+                year_to_df[year] = (
+                    data[["Country", year, 'Coffee type']]
+                    .rename(columns={
+                        year: 'Coffee_Consumption', 'Coffee type': 'Coffee_Type'
+                    })
+                )
 
         data = cls(data=year_to_df)
         LOGGER.info(f"Loaded data for years: {data.get_years()}")
@@ -250,7 +257,7 @@ class SchoolingDataset(BaseDataset):
         columns = ['ISO3', 'Country', 'Continent', 'Hemisphere',
                 'Human Development Groups', 'UNDP Developing Regions',
                 'HDI Rank (2021)', f'Expected Years of Schooling ({year_str})']
-        
+
         return self._data[columns]
 
     @classmethod
@@ -264,7 +271,7 @@ class SchoolingDataset(BaseDataset):
 
         csv_names = os.listdir(path)
         LOGGER.debug(f"Found CSV files: {csv_names}")
-        
+
         assert len(csv_names) > 0, (
             "No CSV files found in the downloaded dataset."
         )
@@ -272,7 +279,7 @@ class SchoolingDataset(BaseDataset):
             "Expected 1 CSV file in the downloaded dataset. "
             f"Got {len(csv_names)}."
         )
-        
+
         data = pd.read_csv(f'{path}/{csv_names[0]}')
         data = cls(data)
         return data
@@ -313,14 +320,14 @@ class StarbucksDataset(BaseDataset):
 
         csv_names_1 = os.listdir(path_1)
         LOGGER.debug(f"Found CSV files 1: {csv_names_1}")
-        
+
         csv_names_2 = os.listdir(path_2)
         LOGGER.debug(f"Found CSV files 2: {csv_names_2}")
-    
-        
+
+
         assert len(csv_names_2) > 0, (
             "No CSV files found in the downloaded dataset."
-        ) 
+        )
         assert len(csv_names_2) == 1, (
             "Expected 1 CSV file in the downloaded dataset. "
             f"Got {len(csv_names_2)}."
@@ -332,11 +339,11 @@ class StarbucksDataset(BaseDataset):
             "Expected 1 CSV file in the downloaded dataset. "
             f"Got {len(csv_names_1)}."
         )
-        
-        
+
+
         data_2021 = pd.read_csv(f'{path_1}/{csv_names_1[0]}')
         data_2017 = pd.read_csv(f'{path_2}/{csv_names_2[0]}')
-        
+
         data = {'2021': data_2021, '2017': data_2017}
         data = cls(data)
         return data
@@ -363,7 +370,7 @@ class IQDataset(BaseDataset):
 
         csv_names = os.listdir(path)
         LOGGER.debug(f"Found CSV files: {csv_names}")
-        
+
         assert len(csv_names) > 0, (
             "No CSV files found in the downloaded dataset."
         )
@@ -371,7 +378,8 @@ class IQDataset(BaseDataset):
             "Expected 1 CSV file in the downloaded dataset. "
             f"Got {len(csv_names)}."
         )
-        
+
         data = pd.read_csv(f'{path}/{csv_names[0]}')
         data = cls(data)
         return data
+
